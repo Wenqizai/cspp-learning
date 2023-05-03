57e0bc6771dce4dc737ac5413247bdaf2ded6b8e
liangwenqi <wenqizai@qq.com>
2023-05-03 19:39:39 +0800

    chapter2-264~266-0503

---
+++ chapter2/264any_odd_one.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <assert.h>
+
+int any_odd_one(unsigned x) {
+	return !!(0xAAAAAAAA & x);
+}
+
+int main() {
+	assert(any_odd_one(0x2));
+	assert(!any_odd_one(0x4));
+	return 0;
+}
+
