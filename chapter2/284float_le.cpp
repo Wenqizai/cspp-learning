4d746b90993eac527ca490e75b907623b6b0efbc
liangwenqi <wenqizai@qq.com>
2023-06-03 12:15:32 +0800

    chapter-284重新实现-0603

--- chapter2/284float_le.cpp
+++ chapter2/284float_le.cpp
@@ -15,11 +15,33 @@ int float_le(float x, float y) {
     unsigned sx = ux >> 31;
     unsigned sy = uy >> 31;
 
-    // ref: https://github.com/DreamAndDead/CSAPP-3e-Solutions/issues/1
-    return (ux << 1 == 0 && uy << 1 == 0) || /* both zeros */
-        (sx && !sy) ||                         /* x < 0, y >= 0 or x <= 0, y > 0 */
-        (!sx && !sy && ux <= uy) ||            /* x > 0, y >= 0 or x >= 0, y > 0 */
-        (sx && sy && ux >= uy);                /* x < 0, y <= 0 or x <= 0, y < 0 */
+    unsigned ex = ux << 1 >> 23;
+    unsigned ey = uy << 1 >> 23;
+
+    // +-0情况
+    if ((ex & ey) == 0) {
+        return 1;
+    }
+
+    // x 负数 y 正数
+    if (sx > sy) {
+        return 1;
+    }
+
+    // x, y 正数
+    if (sx == 0) {
+        return ux - uy;
+    }
+
+    // sx == 1 =>  x,y 负数
+    return !(ux - uy);
+
+
+    //// ref: https://github.com/DreamAndDead/CSAPP-3e-Solutions/issues/1
+    //return (ux << 1 == 0 && uy << 1 == 0) || /* both zeros */
+    //    (sx && !sy) ||                         /* x < 0, y >= 0 or x <= 0, y > 0 */
+    //    (!sx && !sy && ux <= uy) ||            /* x > 0, y >= 0 or x >= 0, y > 0 */
+    //    (sx && sy && ux >= uy);                /* x < 0, y <= 0 or x <= 0, y < 0 */
 }
 
 int main(int argc, char* argv[]) {
@@ -31,4 +53,4 @@ int main(int argc, char* argv[]) {
     assert(float_le(-4, -0));
     assert(float_le(-4, 4));
     return 0;
-}
+} 
